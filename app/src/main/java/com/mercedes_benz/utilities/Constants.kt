b288eb7b113237db969d9049f5ddf62e897606f3
MohammadAbdulah <mohansary98@gmail.com>
2021-07-08 14:59:29 +0300

    Add constants attributes.

--- app/src/main/java/com/mercedes_benz/utilities/Constants.kt
+++ app/src/main/java/com/mercedes_benz/utilities/Constants.kt
@@ -2,3 +2,4 @@ package com.mercedes_benz.utilities
 
 const val PHONE_NUMBER_DIAL = "065805050"
 const val PHONE_NUMBER_WHATSAPP = "0795899084"
+const val NEW_VEHICLES_API = "http://tgfjordan.com:4513/api/FilterList"
